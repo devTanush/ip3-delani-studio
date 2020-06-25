0d4cef71d955178513a0761cdb16d1a8e6744069
nicholastanui@gmail.com <ntanui@safaricom.co.ke>
2020-06-25 23:07:34 +0300

    complete what we do js section

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".design").click(function() {
+        $(".design-main").toggle(250);
+        $(".design-detail").toggle(250);
+        // $("body").addClass("green-background");
+    });
+
+    $(".dev").click(function() {
+        $(".dev-main").toggle(250);
+        $(".dev-detail").toggle(250);
+        // $("body").addClass("green-background");
+    });
+
+    $(".product").click(function() {
+        $(".product-main").toggle(250);
+        $(".product-detail").toggle(250);
+        // $("body").addClass("green-background");
+    });
+
+});
